00bc595e7ba15f5137e15ac236ebee93ea8e1a9a
Alejandro Largo <largoalejo@gmail.com>
2023-03-12 22:09:32 -0500

    Beans Spring Framework

---
+++ src/main/java/com/example/Calculadora.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Calculadora {
+
+    public Calculadora() {
+        System.out.println("Ejecutando calculator service");
+    }
+    public String holaMundo(){
+        return "Hola mundo!";
+    }
+}
---
+++ src/main/java/com/example/GestorFacturas.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class GestorFacturas {
+
+    Calculadora calculadora;
+    String nombre;
+    public GestorFacturas(Calculadora calculadora, String nombre){
+        System.out.println("Ejecutando constructor GestorFacturas");
+        this.calculadora = calculadora;
+        this.nombre = nombre;
+    }
+}
